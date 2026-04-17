88f149460f80d8d17f087f009337d8d2667b10aa
isenf <ffabrodante@gmail.com>
2026-04-17 15:09:38 -0300

    add: listas circulares

---
+++ indices-nodos-recursao/lista-encadeada-simples/CircleList.java
@@ -0,0 +1,149 @@
+/** Lista encadeada circular com nodos do tipo Node que armazenam strings */
+public class CircleList<T extends Comparable<T>> {
+
+    protected Node<T> cursor;	// o cursor corrente 
+    protected int size;	// a quantidade de nodos da lista
+
+    /**
+     * Construtor que cria uma lista vazia
+     */
+    public CircleList() {
+        cursor = null;
+        size = 0;
+    }
+
+    /**
+     * Retorna o tamanho corrente
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Retorna o cursor
+     */
+    public Node<T> getCursor() {
+        return cursor;
+    }
+
+    /**
+     * retorna o tamanho
+     */
+    public int getSize(){
+        return size;
+    }
+
+    /**
+     * Move o cursor adiante
+     */
+    public void advance() {
+        cursor = cursor.getNext();
+    }
+
+    /**
+     * Acrescenta um nodo depois do cursor
+     */
+    public void add(Node<T> newNode) {
+        if (cursor == null) {	// a lista está vazia 
+            newNode.setNext(newNode);
+            cursor = newNode;
+        } else {
+            newNode.setNext(cursor.getNext());
+            cursor.setNext(newNode);
+        }
+        size++;
+    }
+
+    /**
+     * Remove o nodo que segue o cursor
+     */
+    public Node<T> remove() {
+        Node<T> oldNode = cursor.getNext(); // o nodo sendo removido 
+        if (oldNode == cursor) {
+            cursor = null; // a lista se torna vazia 
+        } else {
+            cursor.setNext(oldNode.getNext()); // desconecta o nodo antigo
+            oldNode.setNext(null);
+        }
+        size--;
+        return oldNode;
+    }
+
+    /* 3) Escreva um método que receba duas listas circulares, L e M como parâmetros de entrada, isto é, 
+    duas listas de nodos de forma que cada nodo possui uma referência next não nula. Implemente um algoritmo 
+    para verificar se L e M são na verdade a mesma lista de nodos, mas elas podem ter diferentes (cursores) 
+    pontos de partida.*/
+
+    /** verifica se duas listas circulares são iguais ou não.
+     *  
+     * @return true, se são iguais. false caso contrário.
+     */
+    public static <T extends Comparable<T>> boolean isEqual(CircleList<T> L, CircleList<T> M){
+        if(M.getSize() != L.getSize()) return false;
+
+        SLinkedList<T> list_L =  new SLinkedList<>();
+        SLinkedList<T> list_M = new SLinkedList<>();
+
+        Node<T> aux = L.cursor;
+        do{
+            list_L.addFirst(new Node<T>(L.cursor.getElement(), null));
+            list_M.addFirst(new Node<T>(M.cursor.getElement(), null));
+
+            L.advance();
+            M.advance();
+        } while(L.cursor != aux);
+
+        list_L.insertionSort();
+        list_M.insertionSort();
+
+        Node<T> cur_L = list_L.head, cur_M = list_M.head;
+        while(cur_L != null && cur_M != null){
+            if(cur_L.getElement().compareTo(cur_M.getElement()) != 0) return false;
+
+            cur_L = cur_L.getNext();
+            cur_M = cur_M.getNext();
+        }
+
+        return true;
+    }
+
+    /**
+     * Retorna uma representação string da lista, iniciando pelo cursor
+     */
+    public String toString() {
+        if (cursor == null) {
+            return "[ ]";
+        }
+        String s = "[..." + cursor.getElement();
+        Node<T> oldCursor = cursor;
+        for (advance(); oldCursor != cursor; advance()) {
+            s += ", " + cursor.getElement();
+        }
+        return s + "...]";
+    }
+
+    public static void main(String[] args) {
+        java.util.Random rand = new java.util.Random(67);
+        CircleList<Integer> l1 = new CircleList<>();
+        CircleList<Integer> l2 = new CircleList<>();
+
+        for(int i = 0; i < 10; i++){
+            int num = rand.nextInt(100);
+            l1.add(new Node<>(num, null));
+            //num = rand.nextInt(100);      // para listas diferentes
+            l2.add(new Node<>(num, null));
+        }
+
+        l1.advance();
+        l1.advance();
+        l1.advance();
+        l1.advance();
+
+        //l1.add(new Node<>(rand.nextInt(100), null));  // para tamanhos diferentes
+        System.out.println("l1: " + l1);
+        System.out.println("l2: " + l2);
+
+        System.out.printf("\nl1 e l2 são iguais? %b", isEqual(l1, l2));
+    }
+
+}
