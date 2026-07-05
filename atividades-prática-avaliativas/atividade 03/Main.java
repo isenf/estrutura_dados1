51b75d91a8402edd6bbb4ba222618a9abe46ab18
Fabro <ffabrodante@gmail.com>
2026-07-04 22:46:02 -0300

    feat: adiciona operações e métodos  as operações

--- atividades-prática-avaliativas/atividade 03/Main.java
+++ atividades-prática-avaliativas/atividade 03/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 /**
  * classe executável
  * 
@@ -8,12 +9,39 @@ public class Main{
     private static Scanner sc = new Scanner(System.in);
     private static ExpressionTree expTree = new ExpressionTree();
 
+    /** lê e retorna um inteiro válido */
+    public static int readInt(){
+        while(!sc.hasNextInt()){
+            System.out.print("-> ");
+            sc.next();
+        }
+
+        int val = sc.nextInt();
+        sc.nextLine();
+
+        return val;
+    }
+
+    public static String readString(){
+        String val = sc.nextLine().trim();  
+        if(val.isEmpty()){
+            System.out.print("-> ");
+            return readString();
+        }
+
+        return val;
+    }
+
     /** imprime um menu de operações */
     public static void menu(){
-        System.out.println("\n----- Selecione uma opção -----");
+        System.out.println("\n\n----- Selecione uma opção -----");
         System.out.println("1. Adicionar nova expressão aritmética");
         System.out.println("2. Resolver uma expressão existente");
         System.out.println("3. Imprimir representação da árvore");
+        System.out.println("4. Percurso preorder");
+        System.out.println("5. Percurso inorder");
+        System.out.println("6. Percurso postorder");
+        System.out.println("7. EulerTour");
         System.out.println("0. Sair");
     }
 
@@ -35,11 +63,61 @@ public class Main{
 
     /** resolve a expressão */
     public static void solveExpression(){
+        if(expTree.isEmpty()){
+            System.out.println("erro: a árvore não foi montada");
+            return;
+        }
         System.out.println(expTree.expression + " = " + expTree.evaluate());
     }
 
+    /** imprime a representação de árvore */
     public static void printTree(){
+        if(expTree.isEmpty()){
+            System.out.println("erro: a árvore não foi montada");
+            return;
+        }
 
+        expTree.draw();
+    }
+
+    /** imprime a representação do percurso preorder */
+    public static void showPreorder(){
+        if(expTree.isEmpty()){
+            System.out.println("erro: a árvore não foi montada");
+            return;
+        }
+
+        System.out.println(expTree.stringPreorder(expTree.root));
+    }
+
+    /** imprime a representação do percurso inorder */
+    public static void showInorder(){
+        if(expTree.isEmpty()){
+            System.out.println("erro: a árvore não foi montada");
+            return;
+        }
+
+        System.out.println(expTree.stringInorder(expTree.root));
+    }
+
+    /** imprime a representação do percurso postorder */
+    public static void showPostorder(){
+        if(expTree.isEmpty()){
+            System.out.println("erro: a árvore não foi montada");
+            return;
+        }
+
+        System.out.println(expTree.stringPostorder(expTree.root));
+    }
+
+    /** imprime a representação do euler tour */
+    public static void showEulerTour(){
+        if(expTree.isEmpty()){
+            System.out.println("erro: a árvore não foi montada");
+            return;
+        }
+
+        System.out.println(expTree.stringEulerTour(expTree.root));
     }
 
     public static void main(String[] args) {
@@ -48,47 +126,50 @@ public class Main{
         do{
             menu();
             System.out.print("-> ");
-            option = sc.nextInt();
-            if(option == 0) break;
+            option = readInt();
 
             switch (option) {
-                case 1:
-                    System.out.print("Insira a expressão \n-> ");
-                    sc.nextLine();
-                    String exp = sc.nextLine();
+                case 0:
+                    System.out.println("\nSaindo...");
+                    break;
 
-                    if(exp.isEmpty() || exp == null){
-                        System.out.println("erro: expressão vazia!");
-                        break;
-                    }
+                case 1:
+                    System.out.print("Insira a expressão, ex:((5+4)*(3-2))\n-> ");
+                    String exp = readString();
 
                     ExpressionTree res = addExpression(new ExpressionTree(), exp);
                     if(res != null) expTree = res;
-
                     break;
                 
                 case 2:
-                    if(expTree.isEmpty()){
-                        System.out.println("erro: a árvore não foi montada");
-                        break;
-                    }
-                    
                     solveExpression();
                     break;
             
                 case 3:
-                    if(expTree.isEmpty()){
-                        System.out.println("erro: a árvore não foi montada");
-                        break;
-                    }
+                    printTree();
+                    break;
 
-                    System.out.println(expTree.treeExpression());
+                case 4:
+                    showPreorder();
+                    break;
+
+                case 5:
+                    showInorder();
+                    break;
+
+                case 6:
+                    showPostorder();
+                    break;
+                
+                case 7:
+                    showEulerTour();
                     break;
                     
                 default:
-                    System.out.println("Opção inválida!");
+                    System.out.println("Opção inválida! Tente novamente");
                     break;
             }
+
         } while(option != 0);
 
     }
